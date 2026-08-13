76c8ab127083d016a8f25dca9eecdbddbfbde943
Ebenezer Agonafer <eagonafer@gospel-forum.de>
2026-08-13 16:34:31 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-window.MISSION_CONFIG = {
-  supabaseUrl: "https://cffczdghqnctcketzzoa.supabase.co",
-  supabaseKey: "sb_publishable_HhLuuMPQzOsBNK5YnrY22Q_7WDVg2s_",
-  storageBucket: "content-images"
-};
